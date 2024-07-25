a1d0d45ce408fefee3d10eae587f94c0e9e2bfb8
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-07-25 23:04:11 +0900

    2번째 듣는중인데 확실히 첨 들을때보다 훨 쉽게 들을 수 있는거 같다. 역시 재밌다

---
+++ app.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const welcomeText = document.querySelector("#welcome");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greetings(username);
+}
+
+function greetings(username) {
+  welcomeText.innerText = `Hello ${username}`;
+  welcomeText.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onSubmit);
+} else {
+  greetings(savedUsername);
+}
